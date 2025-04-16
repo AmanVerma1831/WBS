1d7dc1c5fbef7e3ba54b332dddace08150fd92bb
Aman.Verma <Aman.Verma@leanplatformtechnologies.com>
2025-04-16 14:42:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='text-bold text-red-600'>
+          Aman
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
